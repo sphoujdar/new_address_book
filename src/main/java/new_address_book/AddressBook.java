145f3f2033145f70620be139669b02ad3536ad4e
Shubham <sm.phoujdar@gmail.com>
2021-03-15 07:47:49 +0530

    Added : Added and split some functions to modify contacts. (followed DRY principle)

--- src/main/java/new_address_book/AddressBook.java
+++ src/main/java/new_address_book/AddressBook.java
@@ -14,8 +14,28 @@ public class AddressBook {
 	}
 	
 
-	public void addContact (Scanner sc) {
-		System.out.println("Enter Details of Contact you want to add -");
+	public void displayBook(AddressBook passedAddressBook) {
+		
+		System.out.printf("\n\nAddress Book Name is %s. \n\n", passedAddressBook.currentAddressBookName);
+		Contact blankContact = new Contact(null, null, null, null, null, null, 0, 0);
+		
+		System.out.printf("\n\n----------------------------------------------------\n\n");
+		for (int i = 0 ; i<passedAddressBook.currentAddressBook.size(); i++) {
+			System.out.printf("Contact %d :\n",i+1);
+			
+			/*Query - How can I use the displayContact() function 
+			w/o having to make a blank object to access it?*/
+			
+			blankContact.displayContact(passedAddressBook.currentAddressBook.get(i));
+			
+			System.out.printf("\n\n----------------------------------------------------\n\n");
+			
+		}
+		
+	}
+	
+	
+	public Contact createContact (Scanner sc) {
 		
 		System.out.println("Please Enter First Name");
 		String firstName = sc.next();
@@ -37,32 +57,75 @@ public class AddressBook {
 		
 		Contact currentContact = new Contact(firstName,lastName, address, city, state, 
 											 emailAddress, zipCode, phoneNumber);
-		this.currentAddressBook.add(currentContact);	
+		
+		return currentContact;
 		
 	}
 	
 	
-	
-	
-	public void displayBook(AddressBook passedAddressBook) {
+	public void addContact (Scanner sc) {
+		System.out.println("Enter Details of the new Contact you want to add -");
 		
-		System.out.printf("Address Book Name is %s. \n\n", passedAddressBook.currentAddressBookName);
-		Contact blankContact = new Contact(null, null, null, null, null, null, 0, 0);
+			Contact newContact = createContact(sc);
+		
+			this.currentAddressBook.add(newContact);	
+		
+	}
+	
+	
+	public int findContact(Scanner sc) {
 		
 		System.out.printf("\n\n----------------------------------------------------\n\n");
-		for (int i = 0 ; i<passedAddressBook.currentAddressBook.size(); i++) {
-			System.out.printf("Contact %d :\n",i+1);
+		
+		System.out.println("Please Enter First Name of Contact to find :");
+		String fName = sc.next();
+		
+		System.out.println("Please Enter Last Name of Contact to find :");
+		String lName = sc.next();
+		
+		for (int i = 0 ; i<currentAddressBook.size(); i++) {
 			
-			/*Query - How can I use the displayContact() function 
-			w/o having to make a blank object to access it?*/
-			
-			blankContact.displayContact(passedAddressBook.currentAddressBook.get(i));
-			
-			System.out.printf("\n\n----------------------------------------------------\n\n");
-			
+			if(currentAddressBook.get(i).firstName.equals(fName)
+			   && currentAddressBook.get(i).lastName.equals(lName)) {
+				
+				System.out.printf("\nContact is at [%d] position in %s.\n", 
+						          i+1, this.currentAddressBookName);
+				System.out.printf("\n\n----------------------------------------------------\n\n");
+				
+				return i;
+			}
 		}
 		
+		System.out.println("\nContact not in Address Book.\n");
+		System.out.printf("\n\n----------------------------------------------------\n\n");
+		
+		return -999;
 	}
+	
+	
+	public void modifyContact(Scanner sc, AddressBook book1) {
+		int index = book1.findContact(sc);
+		if(index == -999) {
+			return;
+		}
+		else {
+			System.out.println("Enter the updated Contact Details : ");
+
+			Contact updatedContact = createContact(sc);
+			book1.currentAddressBook.set(index, updatedContact);
+			
+			System.out.println("Enter the updated Contact Details : ");
+			
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	/*
 	public AddressBook addNewAddressBook(Scanner sc) {
--- src/main/java/new_address_book/AddressBookMain.java
+++ src/main/java/new_address_book/AddressBookMain.java
@@ -1,3 +1,13 @@
+/*Query 1 - How can I use the displayContact() function 
+			w/o having to make a blank object to access it?*/
+
+/*Query 2 - Where and how exactly were interfaces supposed to 
+			be used in this program?*/
+
+/*Query 3 - How to enter String arguments in console with spaces 
+			included? eg. "7 Suramya" as one address field.*/
+
+
 package new_address_book;
 
 import java.util.Scanner;
@@ -12,6 +22,15 @@ public class AddressBookMain {
 		AddressBook book1 = new AddressBook("Book1");
 		book1.addContact(sc);
 		book1.addContact(sc);
+		book1.displayBook(book1);
+		
+		
+		
+		
+		book1.modifyContact(sc, book1);
+		
+		
+		
 		
 		book1.displayBook(book1);
 		
